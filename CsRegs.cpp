37f79a08e0253ac03cbfc0e0f138b14e8927d94f
Joseph Rahmeh <jrahmeh@tenstorrent.com>
2021-10-22 20:54:40 +0000

    Added vgsrc csr. Changed  reset value of cfg csr.

--- CsRegs.cpp
+++ CsRegs.cpp
@@ -1782,6 +1782,8 @@ CsRegs<URV>::legalizeMhpmevent(CsrNumber number, URV value)
   else
     value = event;
 
+  if (eventRemap_.count(
+
   unsigned counterIx = unsigned(number) - unsigned(CsrNumber::MHPMEVENT3);
   assignEventToCounter(event, counterIx, enableUser, enableMachine);
 
