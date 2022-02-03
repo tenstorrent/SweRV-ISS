f3e64a86552c7f05fc4b5232f6e43eb08d2474f8
Joseph Rahmeh <jrahmeh@tenstorrent.com>
2022-02-03 20:48:32 +0000

    Fixed spelling.

--- Hart.hpp
+++ Hart.hpp
@@ -3767,7 +3767,7 @@ namespace WdRiscv
     uint64_t lrCount_ = 0;    // Count of dispatched load-reserve instructions.
     uint64_t lrSuccess_ = 0;  // Count of successful LR (reservaton acquired).
     uint64_t scCount_ = 0;    // Count of dispatched store-conditional instructions.
-    uint64_t scSuccess_ = 0;  // Counte of successful SC (store accomplished).
+    uint64_t scSuccess_ = 0;  // Count of successful SC (store accomplished).
     unsigned lrResSize_ = sizeof(URV); // LR reservation size.
     bool forceAccessFail_ = false;  // Force load/store access fault.
     bool forceFetchFail_ = false;   // Force fetch access fault.
