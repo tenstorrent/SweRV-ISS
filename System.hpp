45762a94407893609bf4056b6e0b207ae7c49127
Joseph Rahmeh <jrahmeh@tenstorrent.com>
2022-02-07 16:43:55 +0000

    Additional work on memory model: Hooking Mcm to System.

--- System.hpp
+++ System.hpp
@@ -141,6 +141,13 @@ namespace WdRiscv
     /// contents of accessed pages.
     bool writeAccessedMemory(const std::string& path) const;
 
+    /// Enable memory consistency model. This is relevant in
+    /// server/interactive where RTL monitor or interactive command
+    /// may initiate out of order memory transactions. Behavior is
+    /// undefined if used in non-server/non-interactive mode or if
+    /// used after execution has started.
+    void enableMcm(unsigned mergeBufferSize);
+
     bool mcmRead(Hart<URV>& hart, uint64_t time, uint64_t tag, uint64_t addr,
 		 unsigned size, uint64_t data, bool internal);
 
