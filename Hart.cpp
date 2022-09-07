6309af898d1c56b4bd061b4928586d2a3f220f74
Joseph Rahmeh <jrahmeh@tenstorrent.com>
2022-09-06 19:28:37 -0500

    Make printing page table walk information in the CSV log file not conditional on config file.

--- Hart.cpp
+++ Hart.cpp
@@ -3261,7 +3261,7 @@ Hart<URV>::printInstCsvTrace(const DecodedInst& di, FILE* out)
     {
       traceHeaderPrinted_ = true;
       fprintf(out, "pc, inst, modified regs, source operands, memory, inst info, privilege, trap, disassembly, hartid");
-      if (isRvs() and tracePtw_)
+      if (isRvs())
 	fprintf(out, ", iptw, dptw");
       fprintf(out, "\n");
     }
@@ -3457,7 +3457,7 @@ Hart<URV>::printInstCsvTrace(const DecodedInst& di, FILE* out)
   fprintf(out, ",%x", sysHartIndex());
 
   // Page table walk.
-  if (isRvs() and tracePtw_)
+  if (isRvs())
     {
       fputs(",", out);
       std::vector<uint64_t> addrs, entries;
