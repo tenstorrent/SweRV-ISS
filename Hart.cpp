19ed7969980f3cfaca8d797ad7478ce21416ffee
Joseph Rahmeh <jrahmeh@tenstorrent.com>
2022-11-22 14:48:44 -0600

    Read from tohost always returns a zero.

--- Hart.cpp
+++ Hart.cpp
@@ -1478,6 +1478,12 @@ Hart<URV>::load(uint64_t virtAddr, uint64_t& data)
       return true;
     }
 
+  if (toHostValid_ and addr1 == toHost_)
+    {
+      data = 0;
+      return true;
+    }
+
   ULT narrow = 0;   // Unsigned narrow loaded value
   if (addr1 >= clintStart_ and addr1 < clintLimit_ and addr1 - clintStart_ >= 0xbff8)
     {
@@ -1635,6 +1641,10 @@ template <typename STORE_TYPE>
 void
 Hart<URV>::handleStoreToHost(URV physAddr, STORE_TYPE storeVal)
 {
+  ldStWrite_ = true;
+  ldStData_ = storeVal;
+  memory_.write(hartIx_, physAddr, storeVal);
+
   uint64_t val = storeVal;
   uint64_t data = (val << 16) >> 16;
   unsigned cmd = (val >> 48) & 0xff;
@@ -1661,18 +1671,9 @@ Hart<URV>::handleStoreToHost(URV physAddr, STORE_TYPE storeVal)
     }
   else if (dev == 0 and cmd == 0)
     {
-      ldStWrite_ = true;
-      ldStData_ = storeVal;
-      memory_.write(hartIx_, physAddr, storeVal);
       if (storeVal & 1)
 	throw CoreException(CoreException::Stop, "write to to-host",
 			    toHost_, val);
-    }
-  else
-    {
-      ldStWrite_ = true;
-      ldStData_ = storeVal;
-      memory_.write(hartIx_, physAddr, storeVal);
     }
 }
 
