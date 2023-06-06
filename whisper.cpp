c1c6097a359cee3aa45c41566a591f5441fff36e
Joseph Rahmeh <jrahmeh@tenstorrent.com>
2023-06-06 09:20:30 -0500

    Do not advance mcycle if corresponding bit is set in mcountinhibit.

--- whisper.cpp
+++ whisper.cpp
@@ -276,7 +276,7 @@ void
 printVersion()
 {
   unsigned version = 1;
-  unsigned subversion = 805;
+  unsigned subversion = 806;
   std::cout << "Version " << version << "." << subversion << " compiled on "
 	    << __DATE__ << " at " << __TIME__ << '\n';
 #ifdef GIT_SHA
