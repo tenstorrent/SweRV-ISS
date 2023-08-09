fdd1af45e86700e5acb378df56d750cdf5798393
Joseph Rahmeh <jrahmeh@tenstorrent.com>
2023-08-09 13:28:10 -0500

    Work in progress towards AIA: Implemented Imsic class.

---
+++ Imsic.cpp
@@ -0,0 +1,41 @@
+#include "util.hpp"
+#include "Imsic.hpp"
+
+using namespace TT_IMSIC;
+
+bool
+Imsic::write(uint64_t addr, uint64_t data, unsigned size)
+{
+  if (size != 4)
+    return false;
+
+  uint32_t word = data;
+
+  File* file = nullptr;
+
+  if (mfile_.isCoveredAddress(addr))
+    file = &mfile_;
+  else if (sfile_.isCoveredAddress(addr))
+    file = &sfile_;
+  else
+    for (auto& gfile : gfiles_)
+      if (gfile.isCoveredAddress(addr))
+	file = &gfile;
+
+  if (not file)
+    return true;  // Address is not covered by this imsic
+
+  if (not file->isValidAddress(addr))
+    return false;
+
+  if (addr == file->address())
+    file->setPending(word, true);
+  else if (addr == file->address() + 4)
+    {
+      word = util::byteswap(word);
+      file->setPending(word, true);
+    }
+
+  return true;
+}
+
