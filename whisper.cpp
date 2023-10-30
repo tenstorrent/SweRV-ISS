cf3130dd5279fbed19d015df847440f63afec11a
Joseph Rahmeh <jrahmeh@tenstorrent.com>
2023-10-30 09:04:26 -0500

    Upped version.

--- whisper.cpp
+++ whisper.cpp
@@ -276,7 +276,7 @@ void
 printVersion()
 {
   unsigned version = 1;
-  unsigned subversion = 813;
+  unsigned subversion = 814;
   std::cout << "Version " << version << "." << subversion << " compiled on "
 	    << __DATE__ << " at " << __TIME__ << '\n';
 #ifdef GIT_SHA
