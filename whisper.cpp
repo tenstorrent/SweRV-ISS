1395ab305759bdd1924a233d46dd78dafb3509d7
Jiahan Zhang <jzhang@tenstorrent.com>
2024-02-13 09:48:20 -0600

    upped version

--- whisper.cpp
+++ whisper.cpp
@@ -279,7 +279,7 @@ void
 printVersion()
 {
   unsigned version = 1;
-  unsigned subversion = 831;
+  unsigned subversion = 832;
   std::cout << "Version " << version << "." << subversion << " compiled on "
 	    << __DATE__ << " at " << __TIME__ << '\n';
 #ifdef GIT_SHA
