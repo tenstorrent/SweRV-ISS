7d61a5d02df60b496f59aab0b836362c27bacfef
Jiahan Zhang <jzhang@tenstorrent.com>
2024-04-08 14:11:45 -0500

    Add report step.

--- Session.cpp
+++ Session.cpp
@@ -189,9 +189,6 @@ Session<URV>::configureSystem(const Args& args, const HartConfig& config)
       hart0.setInitialStateFile(initStateFile_);
     }
 
-  if (not args.testSignatureFile.empty())
-    result = system.produceTestSignatureFile(args.testSignatureFile) and result;
-
   return true;
 }
 
@@ -1219,6 +1216,58 @@ Session<URV>::determineRegisterWidth(const Args& args, const HartConfig& config)
     std::cerr << "Using default for xlen: " << xlen << "\n";
   
   return xlen;
+}
+
+
+template <typename URV>
+static
+bool
+reportInstructionFrequency(Hart<URV>& hart, const std::string& outPath)
+{
+  FILE* outFile = fopen(outPath.c_str(), "w");
+  if (not outFile)
+    {
+      std::cerr << "Failed to open instruction frequency file '" << outPath
+		<< "' for output.\n";
+      return false;
+    }
+
+  hart.reportInstructionFrequency(outFile);
+  hart.reportTrapStat(outFile);
+  fprintf(outFile, "\n");
+  hart.reportLrScStat(outFile);
+
+  fclose(outFile);
+  return true;
+}
+
+
+template <typename URV>
+bool
+Session<URV>::cleanup(const Args& args)
+{
+  bool result = true;
+
+  auto& hart0 = *system_->ithHart(0);
+  if (not args.instFreqFile.empty())
+    result = reportInstructionFrequency(hart0, args.instFreqFile) and result;
+
+  if (not args.testSignatureFile.empty())
+    result = system_->produceTestSignatureFile(args.testSignatureFile) and result;
+
+  if (args.reportub)
+    {
+      uint64_t bytes = 0;
+      std::vector<std::pair<uint64_t, uint64_t>> blocks;
+      if (not system_->getSparseMemUsedBlocks(blocks))
+        assert(false && "Not compiled with sparse memory");
+      for (const auto& [_, size] : blocks)
+        bytes += size;
+      std::cerr << "Used blocks: 0x" << std::hex << bytes << std::endl;
+    }
+
+  closeUserFiles();
+  return result;
 }
 
 
