f4e77036c54384feb50111578d50bb1bcca9a07f
Joseph Rahmeh <jrahmeh@tenstorrent.com>
2024-07-08 14:24:06 -0500

    Do no update A/D bit of leaf level PTE in stage1 address translation if the PTE resides in a page where the page pased memory type (PBMT) is non-cacahble or is IO. Take a page fault instead.

--- VirtMem.cpp
+++ VirtMem.cpp
@@ -983,8 +983,9 @@ VirtMem::stage1PageTableWalk(uint64_t address, PrivilegeMode privMode, bool read
       if (accessDirtyCheck_ and (not pte.accessed() or (write and not pte.dirty())))
 	{
 	  // We have a choice:
-	  // A. Page fault
-	  if (faultOnFirstAccess1_)
+	  // A. Page fault (if configured or, if page of PTE is non-cachable or is io).
+	  //       pbmt_ is that of leaf page of g-stage translation.
+	  if (faultOnFirstAccess1_ or pbmt_ != Pbmt::None)
             return stage1PageFaultType(read, write, exec);  // A
 
 	  // Or B
