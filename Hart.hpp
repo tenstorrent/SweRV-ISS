e943c931f5b8289819c02192b20edac34269b4a8
Jiahan Zhang <jzhang@tenstorrent.com>
2024-07-10 10:34:30 -0500

    Check widening destination LMUL-alignment for vwsll.vx/vi.

--- Hart.hpp
+++ Hart.hpp
@@ -3021,8 +3021,8 @@ namespace WdRiscv
 			     unsigned op2, unsigned groupX8);
 
     /// Similar to above but 2 vector operands and 1st operand is wide.
-    bool checkVecOpsVsEmulW0(const DecodedInst* di, unsigned op0, unsigned op1,
-			     unsigned groupX8);
+    [[deprecated]] bool checkVecOpsVsEmulW0(const DecodedInst* di, unsigned op0, unsigned op1,
+			                      unsigned groupX8);
 
     /// Similar to above but ternary and 1st operand is wide.
     bool checkVecTernaryOpsVsEmulW0(const DecodedInst* di, unsigned op0, unsigned op1,
