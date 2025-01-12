93117811da8d0d87fe80fe2c069ae1c32791ee5f
Victor Ying <vying@tenstorrent.com>
2025-01-12 05:29:58 +0000

    Fix build of sample trace reader

--- trace-reader/sample-reader.cpp
+++ trace-reader/sample-reader.cpp
@@ -1,5 +1,4 @@
-#include <TraceReader.hpp>
-#include "Pte.hpp"
+#include "TraceReader.hpp"
 
 
 int
@@ -32,7 +31,7 @@ main(int argc, char* argv[])
 
   std::vector<uint64_t> walk;
 
-  reader->definePageTableMaker(0x100000000, PageTableMaker::Sv57, 4*1024*1024);
+  //reader->definePageTableMaker(0x100000000, WhisperUtil::PageTableMaker::Sv57, 4*1024*1024);
 
   uint64_t nn = 0;
   while (reader->nextRecord(record))
