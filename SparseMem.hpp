7c171f22b51167132981630e9fed1a92dc04ca8c
Evgeny Voevodin <evvoevod@tenstorrent.com>
2025-01-15 00:04:24 +0000

    Merge evvoevod/sparsemem-performance into master

--- SparseMem.hpp
+++ SparseMem.hpp
@@ -21,6 +21,7 @@
 #include <span>
 #include <unordered_map>
 #include <mutex>
+#include "Lock.hpp"
 
 
 namespace WdRiscv
@@ -32,7 +33,7 @@ namespace WdRiscv
   {
   public:
 
-    SparseMem();
+    SparseMem() : pageMapCache_(16384) {};
 
     ~SparseMem();
 
@@ -101,30 +102,70 @@ namespace WdRiscv
     /// it has never been accessed before.
     inline std::vector<uint8_t>& findOrCreatePage(uint64_t pageNum)
     {
-      std::lock_guard<std::mutex> lock(mutex_);
+      std::lock_guard<SpinLock> lock(spinlock_);
+      std::vector<uint8_t>* p = pageMapCache_.find(pageNum);
+      if (p != nullptr) {
+        return *p;
+      }
+
       auto end = pageMap_.end();
       auto iter = pageMap_.find(pageNum);
-      if (iter != end)
-	return iter->second;
+      if (iter != end) {
+        std::vector<uint8_t>& page = iter->second;
+        pageMapCache_.update(pageNum, page);
+    	return page;
+      }
 
       return createPage(pageNum);
     }
 
+  private:
+
+    class PageMapCache {
+        struct entry_t {
+            uint64_t pageNum = 0;
+            std::vector<uint8_t>* page = nullptr;
+        };
+        uint64_t mask_;
+        std::vector<entry_t> cache_;
+
+        public:
+        PageMapCache(uint64_t n) : mask_(n - 1) {
+            assert((n & (n - 1)) == 0 && "Cache size must be a power of 2");
+            cache_.resize(n);
+        }
+
+        inline uint64_t idx(uint64_t pageNum) { return pageNum & mask_; }
+
+        inline std::vector<uint8_t>* find(uint64_t pageNum) {
+            entry_t& e = cache_[idx(pageNum)];
+            if (e.page == nullptr or e.pageNum != pageNum) {
+                return nullptr;
+            }
+            return e.page;
+        }
+
+        inline void update(uint64_t pageNum, std::vector<uint8_t>& page) {
+            uint64_t idx = this->idx(pageNum);
+            cache_[idx].pageNum = pageNum;
+            cache_[idx].page = &page;
+        }
+    };
+
     std::vector<uint8_t>& createPage(uint64_t pageNum)
     {
-      auto iter = pageMap_.find(pageNum);
-      if (iter == pageMap_.end())
-	iter = pageMap_.try_emplace(pageNum, pageSize_, 0).first;
-      return iter->second;
+	  auto iter = pageMap_.try_emplace(pageNum, pageSize_, 0).first;
+      auto& page = iter->second;
+	  pageMapCache_.update(pageNum, page);
+      return page;
     }
-
-  private:
 
     size_t pageSize_ = UINT64_C(4)*1024;
     unsigned pageShift_ = 12;
     unsigned pageMask_ = 0xfff;
 
     std::unordered_map<uint64_t, std::vector<uint8_t>> pageMap_;  // Map address to page
-    std::mutex mutex_;
+    PageMapCache pageMapCache_;
+    SpinLock spinlock_;
   };
 }
